6d93834d8440882da10e362b4355ce9d13ca9b62
mickfenneck <michele.sordo@gmail.com>
2013-06-29 20:17:16 +0200

    Update callback with new matrix, added soundfile

--- soundcallback.py
+++ soundcallback.py
@@ -3,6 +3,11 @@
 #Import delle Librerie
 import RPi.GPIO as GPIO
 import time
+from Nsound import *
+#from Nsound import use
+
+#definisco l'audio PlayBack di libao
+use("libao")
 
 #definsco la numerazione dei pin
 GPIO.setmode(GPIO.BOARD)
@@ -15,25 +20,18 @@ channel4 = 15
 channel5 = 19
 channel6 = 21
 channel7 = 23
+channel8 = 26
 
 #Matrice contenente le frequenze dei vari suoni
 matrix = [
-[ 66, 70, 74, 78, 83, 88, 93, 98],     
-[104,110,117,124,131,139,147,156],  
-[165,175,158,196,208,220,233,247],    
-[262,277,294,311,330,349,370,392],
-[415,440,466,494,523,554,587,622],
-[659,698,740,784,831,880,932,988],
-[1046,0,0,0,0,0,0,0]]
-
-matrix2 = [
-[ 1, 2, 3, 4, 5, 6, 7, 8],
-[ 9,10,11,12,13,14,15,16],
-[17,18,19,20,21,22,23,24],
-[25,26,27,28,29,30,31,32],
-[33,34,35,36,37,38,39,40],
-[41,42,43,44,45,46,47,48],
-[49,-1,-1,-1,-1,-1,-1,-1]]
+[   98,  156, 247, 392, 622, 988,0],     
+[   93,  147, 233, 370, 587, 932,0],  
+[   88,  139, 220, 349, 554, 880,0],    
+[   83,  131, 208, 330, 523, 831,0],    
+[   78,  124, 196, 311, 494, 784,0],   
+[   74,  117, 158, 294, 466, 740,0],    
+[   70,  110, 175, 277, 440, 698,0],  
+[   66,  104, 165, 262, 415, 659, 1046]]
 
 
 #imposto i channels come input (pull down virtuale)
@@ -44,15 +42,25 @@ GPIO.setup(channel4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(channel5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(channel6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(channel7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(channel8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 #definisco il numero di colonna
 column = 0
 
 #program header printed on the screen
 try:
+    #definisco la frequenza a cazzo
+    sr = 22050.0
+    #def. la sinusoide
+    sine = Sine(sr)
+    #definisco il suono
+    suono = sine.generate(0.2,440.0)
+    #definisco il playback 
+    pb = AudioPlayback(sr, 1, 16)
+
     print "Callback reading example for MickFenneck's HighSchool Final Project"
-    print "GPIO IN:  7, 11, 13, 15, 19, 21, 23\n"
-    print "GPIO OUT: 8, 10, 12, 16, 18, 22, 24, 26\n"
+    print "GPIO IN:  7, 11, 13, 15, 19, 21, 23, 28\n"
+    print "GPIO OUT: 8, 10, 12, 16, 18, 22, 24\n"
     raw_input("Premi Enter per far cominciare l'analisi...")
 
 
@@ -72,8 +80,6 @@ try:
             return 5
         elif channel == 24:
             return 6
-        elif channel == 26:
-            return 7
         else:
             return -1
         
@@ -93,21 +99,24 @@ try:
             return 5
         elif channel == 23:
             return 6
+        elif channel == 28:
+            return 7
         else:
             return -1
 
     #definisco la funzione di callback generale
     def callback1(channel):
-        #print 'riga: ', channel, ' colonna : ', column
+        print 'riga: ', channel, ' colonna : ', column
         i = rows(channel)
         j = columns(column)
         if(i==-1 or j==-1):
             print 'Errore nella lettura del tasto'
         else:
-            print 'i = ',(i+1),' j = ',(j+1)
-            print matrix2[i][j]
             print matrix[i][j]
+            suono >> pb
+ 	time.sleep(0.7)
 
+        
     #event detect per i diversi canali (tempo di attesa 150 ms)
     GPIO.add_event_detect(channel1, GPIO.RISING, callback=callback1, bouncetime=150)
     GPIO.add_event_detect(channel2, GPIO.RISING, callback=callback1, bouncetime=150)
@@ -116,10 +125,11 @@ try:
     GPIO.add_event_detect(channel5, GPIO.RISING, callback=callback1, bouncetime=150)
     GPIO.add_event_detect(channel6, GPIO.RISING, callback=callback1, bouncetime=150)
     GPIO.add_event_detect(channel7, GPIO.RISING, callback=callback1, bouncetime=150)
+    GPIO.add_event_detect(channel8, GPIO.RISING, callback=callback1, bouncetime=150)
     
     print "The program will now start..."
     #numero dei pin in uscita
-    pin_out = [8, 10, 12, 16, 18, 22, 24, 26]
+    pin_out = [8, 10, 12, 16, 18, 22, 24]
     #inizializzo i pin come uscite
     for ch_ext in pin_out:
         GPIO.setup(ch_ext, GPIO.OUT,initial=GPIO.LOW)
